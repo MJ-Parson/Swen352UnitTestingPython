30a388d0370c0543d8cd1195ab82d11c5949d304
yp5751 <113635140+yp5751@users.noreply.github.com>
2025-09-25 10:26:31 -0400

    Another two tests, added patches to all, fixed inputs to all

--- tests/test_library_db_interface.py
+++ tests/test_library_db_interface.py
@@ -32,33 +32,51 @@ class TestLibraryDB_API(unittest.TestCase):
         self.assertEqual(result, 1)
         mock_db_instance.insert.assert_called_once()
     
-    # def test_insert_not_patron():
+    @patch('library.library_db_interface.TinyDB')
+    def test_insert_not_patron(self, mock_tinydb):
+        lib_db = library_db_interface.Library_DB()
+        invalid_patron = None
+
+        result = lib_db.insert_patron(invalid_patron)
+        self.assertIsNone(result)
+    
+    # @patch('library.library_db_interface.TinyDB')
+    # def test_insert_copy_patron(self, mock_tinydb):
     #     return
     
-    # def test_insert_copy_patron():
+    # @patch('library.library_db_interface.TinyDB')
+    # def test_get_patron_count(self, mock_tinydb):
     #     return
     
-    # def test_get_patron_count():
+    # @patch('library.library_db_interface.TinyDB')
+    # def test_get_patron_count_before_and_after_update(self, mock_tinydb):
     #     return
     
-    # def test_get_patron_count_before_and_after_update():
+    # @patch('library.library_db_interface.TinyDB')
+    # def test_retrieve_patron_success(self, mock_tinydb):
     #     return
     
-    # def test_retrieve_patron_success():
+    # @patch('library.library_db_interface.TinyDB')
+    # def test_retrieve_patron_invalid(self, mock_tinydb):
     #     return
     
-    # def test_retrieve_patron_invalid():
-    #     return
-    
-    # def test_retrieve_patron_insert_duplicate_id():
+    # @patch('library.library_db_interface.TinyDB')
+    # def test_retrieve_patron_insert_duplicate_id(self, mock_tinydb):
     #     #First I add two patrons with the same ID
 
     #     #Then I check to see what happens when I ask for the result
     #     return
     
-    # def test_close_database():
-    #     return
-    
+    @patch('library.library_db_interface.TinyDB')
+    def test_close_database(self, mock_tinydb):
+        mock_db_instance = MagicMock()
+        mock_tinydb.return_value = mock_db_instance
+
+        lib_db = library_db_interface.Library_DB()
+        lib_db.close_db()
+
+        mock_db_instance.close.assert_called_once()
+
     def tearDown(self):
         #Clean up test database file made to prevent actual files being made
         if os.path.exists(self.test_db_file):
